14522deccdf48730fd9b68ea066704cbae583578
Vivek7038 <vivek888chavan@gmail.com>
2023-06-14 09:59:15 +0530

    changed contact us in listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -141,7 +141,7 @@ export default function Listing() {
                 onClick={() => setContactLandlord(true)}
                 className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
               >
-                Contact Landlord
+                Contact Us
               </button>
             </div>
           )}
